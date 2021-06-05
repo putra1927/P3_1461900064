0a4621620e856925f4ef316afe35d9d79386bcc5
putra1927 <putradwiandrianto21@gmail.com>
2021-06-05 13:45:10 +0700

    default

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\usercontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pasien', [usercontroller::class,'index']);
+Route::get('/pasien/tambah', [usercontroller::class,'tambah']);
+Route::get('/pasien/hapus/{id}', [usercontroller::class,'hapus']);
+Route::post('/pasien/store',[usercontroller::class,'store']);
+Route::get('/pasien/edit/{id}',[usercontroller::class,'edit']);
+Route::post('/pasien/update',[usercontroller::class,'update']);
+Route::get('/pasien/cari',[usercontroller::class,'cari']);
+
+
